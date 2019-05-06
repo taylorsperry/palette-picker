9e6da3bd78bc9d6a03d3a923edfc7f0ba7281737
Taylor Sperry <sperry.taylor@gmail.com>
2019-05-06 14:42:26 -0600

    Add get for all palettes  and add error handling.

--- server.js
+++ server.js
@@ -9,3 +9,28 @@ app.use(express.json());
 
 app.listen(app.get('port'), () => console.log(`App is running on localhost: ${app.get('port')}`));
 
+function sendNotFound(res, message) {
+  return res.status(404).json(message)
+}
+
+app.get('/api/v1/projects', (req, res) => {
+  database('projects').select()
+    .then((projects) => {
+      if(!projects) return sendNotFound(res, 'No saved projects were found.');
+      res.status(200).json(projects);
+    })
+    .catch((error) => {
+      res.status(500).json({ error })
+    });
+});
+
+app.get('/api/v1/palettes', (req, res) => {
+  database('palettes').select()
+    .then((palettes) => {
+      if(!palettes) return sendNotFound(res, 'No saved palettes were found.');
+      res.status(200).json(palettes);
+    })
+    .catch((error) => {
+      res.status(500).json({ error })
+    });
+});
