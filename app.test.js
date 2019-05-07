f57835f2da4088ed9fdd8d80232b01d1b66b3987
Taylor Sperry <sperry.taylor@gmail.com>
2019-05-07 15:37:49 -0600

    delegate tests

--- app.test.js
+++ app.test.js
@@ -5,4 +5,58 @@ import request from 'supertest';
 import app from './app';
 import mockData from './mockData'
 
-des
+describe('/api/v1', () => {
+  beforeEach(async () => {
+    await database.seed.run()
+  })
+
+  describe('GET /projects', () => {
+    it('should return all projects')
+    //Liz
+  })
+
+  describe('GET /palettes', () => {
+    it('should return all palettes')
+    //Taylor
+  })
+
+  describe('GET /projects/:id', () => {
+    it('should get a specific project by id')
+    //Liz
+  })
+
+  describe('GET /projects/:id/palettes', () => {
+    it('should get all of the palettes for a specific project')
+    //Taylor
+  })
+
+  describe('POST /projects', () => {
+    it('should post a new project')
+    //Liz
+  })
+
+  describe('POST /projects/:id/palettes', () => {
+    it('should post a new palette with the correct project_id')
+    //Taylor
+  })
+
+  describe('PUT /projects/:id', () => {
+    it('should update a project')
+    //Liz
+  })
+
+  describe('PUT /palettes/:id', () => {
+    it('should update a palette')
+    //Taylor
+  })
+
+  describe('DELETE /projects/:id', () => {
+    it('should delete a project')
+    //Liz
+  })
+
+  describe('DELETE /palettes/:id', () => {
+    it('should delete a palette')
+    //Taylor
+  })
+})
